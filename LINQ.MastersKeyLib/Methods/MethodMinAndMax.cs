d44ab83194cdac4d6611704ce1fdbd845996d2f7
Pablo Casale <casalepablo83@gmail.com>
2022-05-21 14:18:32 -0300

    Add sum max min avg

---
+++ LINQ.MastersKeyLib/Methods/MethodMinAndMax.cs
@@ -0,0 +1,50 @@
+﻿using LINQ.MastersKeyLib.Printer;
+using LINQ.MastersKeyLib.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ.MastersKeyLib.Methods
+{
+    public class MethodMinAndMax
+    {
+        private readonly IPeopleService peopleService;
+
+        public MethodMinAndMax(IPeopleService peopleService)
+        {
+            Print.Title(nameof(MethodMinAndMax));
+            this.peopleService = peopleService;
+        }
+
+        public void Execute()
+        {
+            var numbers = this.peopleService.Numbers();
+
+            var people = this.peopleService.GetPeople();
+
+            var maxHeight = people.Max(x => x.Height);
+            var minHeight = people.Min(x => x.Height);
+
+            Print.KeyValue(nameof(maxHeight), maxHeight);
+            Print.KeyValue(nameof(minHeight), minHeight);
+
+            var maxWeight = people.Max();
+            Print.KeyValue(nameof(maxWeight), maxWeight);
+
+            var averageNumber = numbers.Average();
+            Print.KeyValue(nameof(averageNumber), averageNumber);
+
+            var averageWeight = people.Average(x => x.Weight);
+            Print.KeyValue(nameof(averageWeight), averageWeight);
+
+            var sumOfNumbers = numbers.Sum();
+            Print.KeyValue(nameof(sumOfNumbers), sumOfNumbers);
+
+            var sumOfWeights = people.Sum(x => x.Weight);
+            Print.KeyValue(nameof(sumOfWeights), sumOfWeights);
+
+        }
+    }
+}
--- LINQ.MastersKeyLib/RepositoryService/PeopleService.cs
+++ LINQ.MastersKeyLib/RepositoryService/PeopleService.cs
@@ -13,23 +13,31 @@ namespace LINQ.MastersKeyLib.RepositoryService
     {
         readonly List<Person> people = new List<Person>()
         {
-            new Person{Id = 1, Name = "Aragorn", Kingdom = Kingdoms.Gondor, Height = 1.88},
-            new Person{Id = 2, Name = "Legolas", Kingdom = Kingdoms.Rivendell, Height = 1.75},
-            new Person{Id = 3, Name = "Gimli", Kingdom = Kingdoms.Erebor, Height = 1.00},
-            new Person{Id = 4, Name = "Sauron", Kingdom = Kingdoms.Mordor, Height = 2.20},
-            new Person{Id = 5, Name = "Saruman", Kingdom = Kingdoms.Isengard, Height = 1.94},
-            new Person{Id = 6, Name = "Frodo", Kingdom = Kingdoms.Shire, Height = 0.60},
-            new Person{Id = 7, Name = "Sam", Kingdom = Kingdoms.Shire, Height = 0.55},
-            new Person{Id = 8, Name = "Gandalf", Kingdom = Kingdoms.Erebor, Height = 1.90},
-            new Person{Id = 9, Name = "Boromir", Kingdom = Kingdoms.Gondor, Height = 1.86},
-            new Person{Id = 10, Name = "Faramir", Kingdom = Kingdoms.Gondor, Height = 1.76},
-            new Person{Id = 11, Name = "Eowyn", Kingdom = Kingdoms.Rohan, Height = 1.66},
-            new Person{Id = 12, Name = "Theoden", Kingdom = Kingdoms.Rohan, Height = 1.90},
+            new Person{Id = 1, Name = "Aragorn", Kingdom = Kingdoms.Gondor, Height = 1.88, Weight = 88},
+            new Person{Id = 2, Name = "Legolas", Kingdom = Kingdoms.Rivendell, Height = 1.75, Weight = 75},
+            new Person{Id = 3, Name = "Gimli", Kingdom = Kingdoms.Erebor, Height = 1.00, Weight = 60},
+            new Person{Id = 4, Name = "Sauron", Kingdom = Kingdoms.Mordor, Height = 2.20, Weight = 120},
+            new Person{Id = 5, Name = "Saruman", Kingdom = Kingdoms.Isengard, Height = 1.94, Weight = 90},
+            new Person{Id = 6, Name = "Frodo", Kingdom = Kingdoms.Shire, Height = 0.60, Weight = 54},
+            new Person{Id = 7, Name = "Sam", Kingdom = Kingdoms.Shire, Height = 0.55, Weight = 58},
+            new Person{Id = 8, Name = "Gandalf", Kingdom = Kingdoms.Erebor, Height = 1.90, Weight = 96},
+            new Person{Id = 9, Name = "Boromir", Kingdom = Kingdoms.Gondor, Height = 1.86, Weight = 90},
+            new Person{Id = 10, Name = "Faramir", Kingdom = Kingdoms.Gondor, Height = 1.76, Weight = 84},
+            new Person{Id = 11, Name = "Eowyn", Kingdom = Kingdoms.Rohan, Height = 1.66, Weight = 72},
+            new Person{Id = 12, Name = "Theoden", Kingdom = Kingdoms.Rohan, Height = 1.90, Weight = 80},
         };
+
+        
 
         public List<Person> GetPeople()
         {
             return people;
+        }
+
+        int[] numbers = { 1, 85, 45, 205, 645, 54, 12, 45, 21, 2, 9, 70, 67 };
+        public int[] Numbers()
+        {
+            return this.numbers;
         }
     }
 }
